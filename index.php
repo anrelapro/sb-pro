1ce86798e6e71a198a6e02cdb6a3e9e4e0c7414c
anrelapro <anrelapro@gmail.com>
2016-03-20 01:06:55 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Bravo!</title>
+  <meta name="description" content="SB-PRO site">
+  <meta name="author" content="Your name">
+  <link rel="stylesheet" href="app/css/main.css">
+  <link rel="icon" type="image/png" href="app/img/favicon.png" />
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<div class="sb-table">
+  <div class="sb-table-row">
+    <p>Header</p>
+  </div>
+  <div class="sb-table-row">
+    <div class="sb-table-cell">
+      <p>Content</p>
+    </div>
+  </div>
+  <div class="sb-table-row">
+    <p>Footer</p>
+  </div>
+</div>
+<body>
+  <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+  <script src="app/js/app.js"></script>
+</body>
+</html>
